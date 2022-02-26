a31f2c9b3eecde1a6e93875469c5291730eb89cb
Kiran <kpinnipati@gmail.com>
2022-02-25 16:15:35 -0800

    Basic Navbar added.

--- website.py
+++ website.py
@@ -1,22 +1,34 @@
 from dash import dcc
 from dash import html
 import dash
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 server = app.server
 
-app.layout = html.Div(
-    [
-        dcc.Input(
-            id="input_sequence",
-            type="text",
-            placeholder="input type text",
-        )
-    ]
-    + [html.Div(id="out-all-types")]
-)
+app.layout = html.Div(children = [
+    html.Div(children = [
+        html.H1(children='CrisprHelper', style={'flex-grow': '4', 'margin-line': '20px'}),
 
+        html.Nav(className = "nav nav-pills", children=[
+            html.A('Home', className="nav-item nav-link btn", href='/apps/Home'),
+            html.A('Contact', className="nav-item nav-link active btn", href='/apps/Contact'),
+            html.A('About', className="nav-item nav-link active btn", href='/apps/About')  
+        ], style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-end', 'gap': '30px', 'margin-inline-end':'20px'})
+    ], style={'display': 'flex', 'align-items': 'center'}),
+    html.Hr()
+    
+    # [
+    #     dcc.Input(
+    #         id="input_sequence",
+    #         type="text",
+    #         placeholder="input type text!",
+    #     )
+    # ]
+    # + [html.Div(id="out-all-types")]
+]
+)
 
 @app.callback(
     Output("out-all-types", "children"),
